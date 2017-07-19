5ce979dd510bb8d8db56ba358281a6b8acde6c5d
rkfg <rkfg@rkfg.me>
2017-07-19 19:51:17 +0300

    Add options to disable read receipts and type notifications

--- vector/src/main/java/im/vector/activity/VectorRoomActivity.java
+++ vector/src/main/java/im/vector/activity/VectorRoomActivity.java
@@ -1748,7 +1748,8 @@ public class VectorRoomActivity extends MXCActionBarActivity implements MatrixMe
      */
     private void handleTypingNotification(boolean isTyping) {
         int notificationTimeoutMS = -1;
-        if (isTyping) {
+        boolean typingNotificationsEnabled = PreferenceManager.getDefaultSharedPreferences(getApplicationContext()).getBoolean(getString(R.string.settings_send_typing_notifications), true);
+        if (isTyping && typingNotificationsEnabled) {
             // Check whether a typing event has been already reported to server (We wait for the end of the local timeout before considering this new event)
             if (null != mTypingTimer) {
                 // Refresh date of the last observed typing
@@ -1822,6 +1823,9 @@ public class VectorRoomActivity extends MXCActionBarActivity implements MatrixMe
             mLastTypingDate = 0;
         }
 
+        if (!typingNotificationsEnabled) {
+            return;
+        }
         final boolean typingStatus = isTyping;
 
         mRoom.sendTypingNotification(typingStatus, notificationTimeoutMS, new SimpleApiCallback<Void>(VectorRoomActivity.this) {
@@ -2319,6 +2323,11 @@ public class VectorRoomActivity extends MXCActionBarActivity implements MatrixMe
      */
     private void onRoomTypings() {
         mLatestTypingMessage = null;
+
+        if (!PreferenceManager.getDefaultSharedPreferences(getApplicationContext()).getBoolean(getString(R.string.settings_show_typing_notifications), true)) {
+            refreshNotificationsArea();
+            return;
+        }
 
         List<String> typingUsers = mRoom.getTypingUsers();
 
