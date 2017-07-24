bd13c4c3d23e21828d0076c4918c8f1e51fa502a
rkfg <rkfg@rkfg.me>
2017-07-24 18:25:45 +0300

    Fix keys

--- vector/src/main/java/im/vector/adapters/VectorMessagesAdapter.java
+++ vector/src/main/java/im/vector/adapters/VectorMessagesAdapter.java
@@ -94,6 +94,7 @@ import im.vector.util.EventGroup;
 public class VectorMessagesAdapter extends AbstractMessagesAdapter {
 
     private static final String LOG_TAG = "VMessagesAdapter";
+    private static final String SETTINGS_FONT_SIZE_KEY = "SETTINGS_FONT_SIZE_KEY";
 
     // an event is selected when the user taps on it
     private String mSelectedEventId;
@@ -1107,7 +1108,7 @@ public class VectorMessagesAdapter extends AbstractMessagesAdapter {
     }
 
     private float getTextAppearance() {
-        String size = PreferenceManager.getDefaultSharedPreferences(mContext).getString(mContext.getString(R.string.settings_font_size), "small");
+        String size = PreferenceManager.getDefaultSharedPreferences(mContext).getString(SETTINGS_FONT_SIZE_KEY, "small");
         switch (size) {
             case "medium":
                 return 18;
